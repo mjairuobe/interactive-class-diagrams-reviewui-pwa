27e3c54e38519e2257f129aacdbb809c0b81978d
Mike Iruobe <mj_iruobe@utlook.de>
2026-04-25 06:13:09 +0200

    fix: remove custom wheel step to restore stable react-zoom-pan-pinch default zoom

--- src/App.tsx
+++ src/App.tsx
@@ -593,8 +593,6 @@ function App() {
           centerOnInit={true}
           limitToBounds={false}
           panning={{ velocityDisabled: false }}
-          wheel={{ step: 0.05 }}
-          doubleClick={{ step: 0.5 }}
         >
           <TransformComponent wrapperStyle={{ width: "100%", height: "100%" }} contentStyle={{ width: "100%", height: "100%" }}>
             <div
