e9ee4a0e02c1ef65e16800cc9cd0dbd8152a32a6
Mike Iruobe <mj_iruobe@utlook.de>
2026-04-25 06:17:39 +0200

    fix: make mouse wheel scroll step even finer

--- src/App.tsx
+++ src/App.tsx
@@ -593,7 +593,7 @@ function App() {
           centerOnInit={true}
           limitToBounds={false}
           panning={{ velocityDisabled: false }}
-          wheel={{ step: 0.005 }}
+          wheel={{ step: 0.002 }}
         >
           <TransformComponent wrapperStyle={{ width: "100%", height: "100%" }} contentStyle={{ width: "100%", height: "100%" }}>
             <div
